c645eec0e7341ed5895e266b77b6ef289d576eef
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-01-27 08:37:48 -0800

    Completed

---
+++ java Internship/Basic Calculatoe/Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        {
+            System.out.println("Enter the First Number");
+            int inputFirstNumber = scanner.nextInt();
+
+            System.out.println("1: Add");
+            System.out.println("2: Subtract");
+            System.out.println("3: Multiplication");
+            System.out.println("4: Division");
+            System.out.println("Choose Operation ");
+            int Operation = scanner.nextInt();
+            System.out.println("Enter the Second  Number");
+            int inputSecondNumber = scanner.nextInt();
+
+            switch (Operation) {
+                case 1:
+                    System.out.println("Result is :" + inputFirstNumber + inputSecondNumber);
+                    break;
+
+                case 2:
+                    int result = inputFirstNumber - inputSecondNumber;
+                    System.out.println("Result is :" + result);
+
+                    break;
+                case 3:
+                    System.out.println("Result is :" + inputFirstNumber * inputSecondNumber);
+
+                    break;
+                case 4:
+                    System.out.println("Result is :" + inputFirstNumber / inputSecondNumber);
+
+                    break;
+            }
+
+        }
+    }
+}
